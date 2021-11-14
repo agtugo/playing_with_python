6db9a07c5f8549bac1c08a71ac06248387573af4
Arturo Muñoz Tolosa <agtugo@gmail.com>
2021-11-14 13:23:41 -0800

    Updated comments of Iterative for to show what is the objective of the class For

--- IterativeFor.py
+++ IterativeFor.py
@@ -37,7 +37,19 @@ class Pool:
     def pool_sum(pools):
         res = sum(pool.value for pool in pools)
         return res
-
+#The objective of this class is to create a callable or an object
+#there are many algorithms that requires many nested fors, 
+#this could be solved with a recursive approach, the recursive 
+#approach could be using calling the function inside the function
+#or modifying the result while working on the result.
+#This is just another approach. The product function from itertools
+#can provide some of this functionality but this would be a generalization
+#in general terms this would be similar to this.
+#def For(container, appendables,action1, action2):
+#    for i in container:
+#	appendables.append(i)#The last for should contain the information of all fors
+#	action1(appendables) #This action1 is supposed to contain an specific function 
+#	action2(appendables) #This will contain the next for in case you need to nest
 @dataclass
 class For:
     container : Collection
